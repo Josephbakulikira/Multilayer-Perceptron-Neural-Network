8cf13c2c3c43c6bbacc0b506fee1f96ae31c4c82
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2021-01-20 06:07:42 +0530

    Feed forward algorithm

--- Matrix.py
+++ Matrix.py
@@ -62,7 +62,7 @@ class Matrix:
     def randomize(self):
         for x in range(self.rows):
             for y in range(self.cols):
-                self.matrix[x][y] = random.randint(0, 10)
+                self.matrix[x][y] = random.uniform(-1, 1)
 
     def map(self, fn):
         #apply a function for every element in the matrix
@@ -70,37 +70,45 @@ class Matrix:
             for y in range(self.cols):
                 self.matrix[x][y] = fn(self.matrix[x][y])
 
-    def matrix_multiplication(a, b):
-        columns_a = len(a[0])
-        rows_a = len(a)
-        columns_b = len(b[0])
-        rows_b = len(b)
+    def fromArray(arr):
+        rows = len(arr)
+        cols = 1
+        current = Matrix(rows, cols)
+        for i in range(rows):
+            current.matrix[i][0] = arr[i]
+        return current
 
-        result_matrix = [[j for j in range(columns_b)] for i in range(rows_a)]
+    def matrix_multiplication(a, b):
+        columns_a = len(a.matrix[0])
+        rows_a = len(a.matrix)
+        columns_b = len(b.matrix[0])
+        rows_b = len(b.matrix)
+
+        result_matrix = Matrix(rows_a, columns_b)
         if columns_a == rows_b:
             for x in range(rows_a):
                 for y in range(columns_b):
                     sum = 0
                     for k in range(columns_a):
-                        sum += a[x][k] * b[k][y]
-                    result_matrix[x][y] = sum
+                        sum += a.matrix[x][k] * b.matrix[k][y]
+                    result_matrix.matrix[x][y] = sum
             return result_matrix
 
         else:
             print("columns of the first matrix must be equal to the rows of the second matrix")
             return None
 
-#testing 
+#testing
 
 # m1 = Matrix(2, 3)
 # m1.matrix=[[2, 3, 4],[5, 4, 2]]
 # m2 = Matrix(3, 2)
 # m2.matrix = [[9, 4], [3, 2], [5,6]]
-#
-#
 # #
-# # m1.matrix = Matrix.matrix_multiplication(m1.matrix, m2.matrix)
-# # m1 = m1.multiply(m2)
+# #
+# # #
+# m1 = Matrix.matrix_multiplication(m1, m2)
+# # # m1 = m1.multiply(m2)
 # print(m1.matrix)
-# #print(m1.rows)
-# # print(m1.cols)
+# # #print(m1.rows)
+# # # print(m1.cols)
--- Network.py
+++ Network.py
@@ -1,8 +1,36 @@
+from Matrix import *
+import math
+
+def Sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
 class NeuralNetwork:
     def __init__(self, n_inputs, n_hiddenLayers, n_outputs):
         self.input_layers = n_inputs
         self.hidden_layers = n_hiddenLayers
         self.output_layers = n_outputs
 
+        self.hiddenInputWeights = Matrix(self.hidden_layers, self.input_layers)
+        self.hiddenInputWeights.randomize()
+        self.hiddenOutputWeights = Matrix(self.output_layers, self.hidden_layers)
+        self.hiddenOutputWeights.randomize()
+
+        self.hiddenBias = Matrix(self.hidden_layers, 1)
+        self.hiddenBias.randomize()
+        self.outputBias = Matrix(self.output_layers, 1)
+        self.outputBias.randomize()
+
+
+
     def FeedForward(inputs):
-        print("here we go again :)")
+        inputs = Matrix.fromArray(inputs)
+
+        hidden = Matrix.matrix_multiplication(self.hiddenInputWeights, inputs)
+        hidden.add(self.hiddenBias)
+        hidden.map(Sigmoid)
+
+        output = Matrix.matrix_multiplication(self.hiddenOutputWeights , hidden)
+        output.add(self.outputBias)
+        output.map(Sigmoid)
+
+        return output
